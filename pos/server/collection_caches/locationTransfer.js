be5d8395e28cab0851764c3ff15afdcd01d9a8a6
phirum <phirumsim@gmail.com>
2016-07-05 10:52:34 +0700

    update i18n

--- pos/server/collection_caches/locationTransfer.js
+++ pos/server/collection_caches/locationTransfer.js
@@ -2,10 +2,9 @@ import 'meteor/theara:collection-cache';
 
 // Collection
 import {LocationTransfers} from '../../imports/api/collections/locationTransfer.js';
-import {Staffs} from '../../imports/api/collections/staff.js';
 import {StockLocations} from '../../imports/api/collections/stockLocation.js';
 
 LocationTransfers.cacheTimestamp();
-LocationTransfers.cacheDoc('fromStaff', Staffs, ['name'],'fromStaffId');
-LocationTransfers.cacheDoc('toStaff', Staffs, ['name'],'toStaffId');
-LocationTransfers.cacheDoc('stockLocation',StockLocations,['name']);
+//LocationTransfers.cacheDoc('fromUser', Staffs, ['name'], 'fromUserId');
+//LocationTransfers.cacheDoc('toUser', Staffs, ['name'], 'toUserId');
+LocationTransfers.cacheDoc('stockLocation', StockLocations, ['name']);
